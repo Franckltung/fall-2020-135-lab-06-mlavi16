6f91776552c80112007d2a37f629703871ec315a
mlavi16 <Maya.lavi11@gmail.com>
2020-10-08 18:17:40 -0400

    finished encryptVigenere()

--- vigenere.cpp
+++ vigenere.cpp
@@ -2,9 +2,40 @@
 #include <cctype>
 #include <string>
 #include "vigenere.h"
+#include "caesar.h"
+
+char keywordEncryption(char c, char key) {
+    int shift = (int)key - 97;
+    char new_c = (int)c + shift;
+    return new_c;
+}
+
+void increment_keyword_ch(int& cur_shift, int max_shift) {
+    if (cur_shift < max_shift) {
+        cur_shift += 1;
+    } else {
+        cur_shift = 0;
+    }
+}
 
 std::string encryptVigenere(std::string plain_text, std::string keyword) {
     std::string encrypted_text = "";
+    int cur_shift = 0;
+    int max_shift = keyword.length() - 1;
+    int shifts [keyword.length()];
+
+    for (int i = 0; i < keyword.length(); i++){
+        shifts[i] = (int)keyword[i] - 97; // a = 97
+    }
+    
+    for (int i = 0; i < plain_text.length(); i++) {
+        if (isalpha(plain_text[i])) {
+            encrypted_text += shiftChar(plain_text[i], shifts[cur_shift]);
+            increment_keyword_ch(cur_shift, max_shift);
+        } else {
+            encrypted_text += plain_text[i];
+        }
+    }
+
     return encrypted_text;
 }
-
--- vigenere.h
+++ vigenere.h
@@ -1,3 +1,5 @@
 #pragma once
 
-std::string encryptVigenere(std::string plaintext, std::string keyword);
+std::string encryptVigenere(std::string plain_text, std::string keyword);
+char keywordEncryption(char c, char key);
+void increment_keyword_ch(int& cur_shift, int max_shift);
