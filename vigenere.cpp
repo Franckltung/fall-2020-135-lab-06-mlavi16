549827949b1ca5fd4502fae5a46fd7f5dea3e357
mlavi16 <Maya.lavi11@gmail.com>
2020-10-08 15:57:29 -0400

    adding encryptVigenere()

---
+++ vigenere.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+#include "vigenere.h"
+
+std::string encryptVigenere(std::string plain_text, std::string keyword) {
+    std::string encrypted_text = "";
+    return encrypted_text;
+}
+
