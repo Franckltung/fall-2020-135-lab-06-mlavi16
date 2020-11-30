1e98dd7f2b50831a470894b5b40db23aa2c0584d
Default Profile <Default Profile>
2020-11-30 15:27:05 -0500

    Fixed issue

--- funcs.cpp
+++ funcs.cpp
@@ -6,8 +6,7 @@
 void test_ascii(std::string str) {
     std::string output = "";
     for (int i = 0; i < str.length(); i++) {        
-        std::cout << str[i] << " ";
-        std::cout << (int)str[i] << "\n";
+        std::cout << str[i] << " " << (int)str[i] << "\n";
     }
     std::cout << output;
 }
